98203123fb09e5247762bba0c8c6131504f32bcc
Alannah Kearney <hi@alannahkearney.com>
2020-11-12 18:12:21 +1000

    feat: Add SymphonyException exception class

---
+++ src/Symphony/Exceptions/SymphonyException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symphony\Symphony\Exceptions;
+
+use pointybeard\Helpers\Exceptions\ReadableTrace;
+
+class SymphonyException extends ReadableTrace\ReadableTraceException
+{
+}
